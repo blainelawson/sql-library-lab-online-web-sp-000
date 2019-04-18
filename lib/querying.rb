665f0507133b5224d0e520deda032e5c1b8e6e9f
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-18 20:46:47 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, max(count(species)) FROM characters"
+  "SELECT species, count(species) FROM characters
+   ORDER BY count(species) DESC
+   LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
