bd492c8f4eacbca54e7d403d13be25b08a7f1858
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-18 21:08:47 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, count(*) AS species_count FROM characters
+  "SELECT species, count(species) AS species_count FROM characters
 "
 end
 
