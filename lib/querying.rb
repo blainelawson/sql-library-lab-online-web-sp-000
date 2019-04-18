1aa46564829ead3743177e8e79660df7f570e11c
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-18 21:42:28 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,9 +27,8 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title, count(characters.species) FROM series
    JOIN characters ON characters.series_id = series.id
-   WHERE GROUP BY characters.species
-   ORDER BY count(characters.species)
-   LIMIT 1"
+   WHERE characters.species = \"human\"
+"
 end
 
 def select_character_names_and_number_of_books_they_are_in
