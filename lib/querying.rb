afb12c4f7104d09c9d365aa8c908e5194ba48521
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-18 21:31:14 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,7 +28,7 @@ def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
    JOIN characters ON characters.series_id = series.id
    GROUP BY characters.species
-   ORDER BY count(characters.species) DESC
+   ORDER BY count(characters.species)
    LIMIT 1"
 end
 
